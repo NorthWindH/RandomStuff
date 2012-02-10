78d5b8580ada9888ac8e72fc979e3fa052897882
Hasan Kamal-Al-Deen <hasank1987@gmail.com>
2012-02-10 08:15:10 -0500

    Began flat access project.

---
+++ FlatAccess/main.cpp
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * FlatAccess
+ *
+ * This file tests whether flat access is faster than other modes of access.
+ */
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
